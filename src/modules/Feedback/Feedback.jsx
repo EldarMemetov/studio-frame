2a2a8569f3e6bfe4621a909f6db5848c611703f1
EldarMemetov <eldarm9347@gmail.com>
2025-07-12 00:07:20 +0200

    add

--- src/modules/Feedback/Feedback.jsx
+++ src/modules/Feedback/Feedback.jsx
@@ -24,71 +24,99 @@ export default function ContactSection() {
   return (
     <section id="contact" className={s.section}>
       <Container>
-        <h2>Feedback</h2>
-        <Formik
-          initialValues={{ name: '', email: '', message: '', agree: false }}
-          validationSchema={FeedbackSchema}
-          onSubmit={async (values, { setSubmitting, resetForm, setStatus }) => {
-            try {
-              const res = await sendFeedback(values);
-              setStatus({ success: res.message });
-              resetForm();
-            } catch (error) {
-              setStatus({ error: error.message });
-            } finally {
-              setSubmitting(false);
-            }
-          }}
-        >
-          {({ isSubmitting, status }) => (
-            <Form className={s.form}>
-              <label>
-                <Field type="text" name="name" placeholder="Name" />
-                <ErrorMessage name="name" component="div" className={s.error} />
-              </label>
+        <div className={s.bigContainer}>
+          <div className={s.stepsContainer}>
+            <span className={s.spanInfo}>Форма</span>
+          </div>
 
-              <label>
-                <Field type="email" name="email" placeholder="Email" />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className={s.error}
-                />
-              </label>
+          <h2 className={s.title}>
+            Залиште заявку — і я зв'яжусь з вами найближчим часом
+          </h2>
+          <Formik
+            initialValues={{ name: '', email: '', message: '', agree: false }}
+            validationSchema={FeedbackSchema}
+            onSubmit={async (
+              values,
+              { setSubmitting, resetForm, setStatus }
+            ) => {
+              try {
+                const res = await sendFeedback(values);
+                setStatus({ success: res.message });
+                resetForm();
+              } catch (error) {
+                setStatus({ error: error.message });
+              } finally {
+                setSubmitting(false);
+              }
+            }}
+          >
+            {({ isSubmitting, status }) => (
+              <Form className={s.form}>
+                <label>
+                  <span>Ім’я</span>
+                  <Field
+                    type="text"
+                    name="name"
+                    placeholder="Електронна адреса"
+                  />
+                  <ErrorMessage
+                    name="name"
+                    component="div"
+                    className={s.error}
+                  />
+                </label>
 
-              <label>
-                <Field
-                  as="textarea"
-                  name="message"
-                  placeholder="Message"
-                  rows={4}
-                />
-                <ErrorMessage
-                  name="message"
-                  component="div"
-                  className={s.error}
-                />
-              </label>
+                <label>
+                  <span>Електронна адреса</span>
+                  <Field
+                    type="email"
+                    name="email"
+                    placeholder="Електронна адреса"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className={s.error}
+                  />
+                </label>
 
-              <label className={s.checkboxLabel}>
-                <Field type="checkbox" name="agree" />
-                I agree to the privacy policy
-                <ErrorMessage
-                  name="agree"
-                  component="div"
-                  className={s.error}
-                />
-              </label>
+                <label>
+                  <span>Текст</span>
+                  <Field
+                    as="textarea"
+                    name="message"
+                    placeholder="Текст"
+                    rows={4}
+                  />
+                  <ErrorMessage
+                    name="message"
+                    component="div"
+                    className={s.error}
+                  />
+                </label>
 
-              <button type="submit" disabled={isSubmitting}>
-                {isSubmitting ? 'Sending...' : 'Send Feedback'}
-              </button>
+                <label className={s.checkboxLabel}>
+                  <Field type="checkbox" name="agree" />
+                  Я погоджуюся з умовами конфіденційності
+                  <ErrorMessage
+                    name="agree"
+                    component="div"
+                    className={s.error}
+                  />
+                </label>
 
-              {status?.success && <p className={s.success}>{status.success}</p>}
-              {status?.error && <p className={s.error}>{status.error}</p>}
-            </Form>
-          )}
-        </Formik>
+                <button type="submit" disabled={isSubmitting}>
+                  {isSubmitting ? 'Sending...' : 'Send Feedback'}
+                </button>
+
+                {status?.success && (
+                  <p className={s.success}>{status.success}</p>
+                )}
+                {status?.error && <p className={s.error}>{status.error}</p>}
+              </Form>
+            )}
+          </Formik>
+        </div>
       </Container>
     </section>
   );
